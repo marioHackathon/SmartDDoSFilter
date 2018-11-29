f8939429c72a44cf14a6281004d8418fbba9b676
Emilio Campos <emilio.campos@zevenet.com>
2018-11-29 20:20:40 +0100

    [Improvement] A real example of how to connect a backend system (Apache2+libmodsecurity) to nftlb
    Signed-off-by: Emilio Campos <emilio.campos@zevenet.com>
    
    	new file:   l7-2-nftlb/README.me
    	new file:   l7-2-nftlb/l7-2-nftlb.lua
    	new file:   l7-2-nftlb/libmod_exec_secrule.conf

---
+++ l7-2-nftlb/l7-2-nftlb.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/lua
+function main()
+
+m.log(1,"Starting Lua Scripting  \n")
+
+--capture remote addr from Connection
+local remote_addr = m.getvar("REMOTE_ADDR") 
+
+---send request to NFTLB with the origin IP. NFTLB remote host 172.16.64.53
+---TODO: use libcurl instead. 
+local sys = os.execute("curl -X PUT -H 'Content-Type: application/json' http://172.16.64.53:5555/policies/l7filter -d '{ policies: [{ \"name\" : \"l7fiter\", \"elements\": [{ \"data\" : \" " ..remote_addr.. " \" }] }] }' ")
+
+end
