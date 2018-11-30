5a466b5f5e04609d09c95ee3207c6ca86167f6fe
Emilio Campos <emilio.campos@zevenet.com>
2018-11-30 10:18:45 +0100

    fixed typo in curl call related to issue #1

--- l7-2-nftlb/l7-2-nftlb.lua
+++ l7-2-nftlb/l7-2-nftlb.lua
@@ -8,6 +8,6 @@ local remote_addr = m.getvar("REMOTE_ADDR")
 
 ---send request to NFTLB with the origin IP. NFTLB remote host 172.16.64.53
 ---TODO: use libcurl instead. 
-local sys = os.execute("curl -X PUT -H 'Content-Type: application/json' http://172.16.64.53:5555/policies/l7filter -d '{ policies: [{ \"name\" : \"l7fiter\", \"elements\": [{ \"data\" : \" " ..remote_addr.. " \" }] }] }' ")
+local sys = os.execute("curl -X PUT -H 'Content-Type: application/json' http://172.16.64.53:5555/policies/l7filter -d '{ policies: [{ \"name\" : \"l7filter\", \"elements\": [{ \"data\" : \" " ..remote_addr.. " \" }] }] }' ")
 
 end
