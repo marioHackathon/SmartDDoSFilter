8dea71f9d090190c963409605886d8e9b8aba1dd
Emilio Campos <emilio.campos@zevenet.com>
2018-12-01 02:35:41 +0100

    [Improvement] Workaround, added ssh tunnel for connecting to nftlb API from libmodsecurity.
    Signed-off-by: Emilio Campos <emilio.campos@zevenet.com>
    
    	modified:   l7-2-nftlb.lua

--- l7-2-nftlb/l7-2-nftlb.lua
+++ l7-2-nftlb/l7-2-nftlb.lua
@@ -1,13 +1,24 @@
 #!/usr/bin/lua
 function main()
 
-m.log(1,"Starting Lua Scripting  \n")
+m.log(1,"Starting l7-2-nftlb Lua Scripting  \n")
 
 --capture remote addr from Connection
 local remote_addr = m.getvar("REMOTE_ADDR") 
 
----send request to NFTLB with the origin IP. NFTLB remote host 172.16.64.53
----TODO: use libcurl instead. 
-local sys = os.execute("curl -X PUT -H 'Content-Type: application/json' http://172.16.64.53:5555/policies/l7filter -d '{ policies: [{ \"name\" : \"l7filter\", \"elements\": [{ \"data\" : \" " ..remote_addr.. " \" }] }] }' ")
+---send request to NFTLB with the origin IP.
+---test purpose
+---local sys = os.execute("curl http://172.16.64.53:5555/torta")
+---it should work
+local sys = os.execute("ssh apiuser@172.16.64.121 \"curl -X PUT -H 'Content-Type: application/json' http://localhost:5555/policies/l7filter -d '{ policies: [{ \"name\" : \"l7fiter\", \"elements\": [{ \"data\" : \" " ..remote_addr.. " \" }] }] }' \" ")
+---local sys = os.execute("curl -X PUT -H 'Content-Type: application/json' http://172.16.64.121:5556/policies/l7filter -d '{ policies: [{ \"name\" : \"l7fiter\", \"elements\": [{ \"data\" : \" " ..remote_addr.. " \" }] }] }' ")
+
+
+--save in log file information for debug purpose
+local fileHandle = assert(io.open('/lua_output.txt','a'))
+fileHandle:write("--- Output start ---\n")
+fileHandle:write(remote_addr) 
+fileHandle:write("\n --- Output end --- \n")
+m.log(1,"Finishing l7-2-nftlb Lua Scripting\n")
 
 end
